e9903b863be346fa569378f51d889a2a007a8ed3
Dustin Van Buskirk <dev@vanbuskirk.me>
2016-03-30 14:35:21 -0700

    bumping cookbook version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'dustin.vanbuskirk@ijettechnologies.com'
 license          'All rights reserved'
 description      'Installs concourse for Vagrant currently'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.2'
+version          '0.0.3'
 
 supports         'centos', '>= 7.0'
 
